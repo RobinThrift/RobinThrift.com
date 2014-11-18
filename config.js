b2ef105698c687565bd24a8fa9a33e24f468d37e
Robin Thrift <robin@we-two-design.com>
2014-11-18 12:11:22 +0100

    no comments in dev

--- config.js
+++ config.js
@@ -3,12 +3,14 @@
 var prod = {
         'sitename': 'Robin Thrift',
         'baseUrl': 'http://www.robinthrift.com',
-        'description': 'My personal blog'
+        'description': 'My personal blog',
+        'isDev': false
     },
     dev = {
         'sitename': 'Robin Thrift (DEV)',
         'baseUrl': 'http://localhost:3000',
-        'description': 'My personal blog'
+        'description': 'My personal blog',
+        'isDev': true
     };
 
 module.exports = function(args) {
