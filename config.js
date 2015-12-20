add2f8475062003e01bb2513f286b6b6bb4b9762
Robin Thrift <thrift.d.robin@googlemail.com>
2015-12-20 14:43:23 +0100

    weekly picks 2

--- config.js
+++ config.js
@@ -1,32 +1,26 @@
-
-
 var prod = {
-        'sitename': 'Robin Thrift',
-        'baseUrl': 'http://www.robinthrift.com',
-        'description': 'My personal blog',
-        'isDev': false,
-        'twitter': 'RobinThrift',
-        'github': 'RobinThrift'
-    },
-    dev = {
-        'sitename': 'Robin Thrift',
-        'baseUrl': 'http://192.168.178.32:3000',
-        'description': 'My personal blog',
-        'isDev': true,
-        'twitter': 'RobinThrift',
-        'github': 'RobinThrift'
-    };
+    'sitename': 'Robin Thrift',
+    'baseUrl': 'http://www.robinthrift.com',
+    'description': 'My personal blog',
+    'isDev': false,
+    'twitter': 'RobinThrift',
+    'github': 'RobinThrift'
+};
+var dev = {
+    'sitename': 'Robin Thrift',
+    'baseUrl': 'http://localhost:3000',
+    'description': 'My personal blog',
+    'isDev': true,
+    'twitter': 'RobinThrift',
+    'github': 'RobinThrift'
+};
 
 module.exports = function(args) {
-    'use strict';
     var config = dev;
-
     args.forEach(function(val) {
         if (val === '--prod' || val === '-p') {
-            config = prod; 
-        } 
+            config = prod;
+        }
     });
-
     return config;
-
 };
