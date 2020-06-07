01ce470ae5b7f67549c3efb0876cb83071486c21
Robin Thrift <services@robinthrift.com>
2020-06-07 12:50:27 +0200

    Switch from travis to GitHub actions for building

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+GHA_BRANCH=${GITHUB_REF#refs/heads/}
+
 # Deploy built docs to this branch
 TARGET_BRANCH=gh-pages
  
@@ -11,56 +13,28 @@ fi
  
 REPO=$(git config remote.origin.url)
  
-if [ -n "$TRAVIS_BUILD_ID" ]; then
-  # When running on Travis we need to use SSH to deploy to GitHub
-  #
-  # The following converts the repo URL to an SSH location,
-  # decrypts the SSH key and sets up the Git config with
-  # the correct user name and email (globally as this is a
-  # temporary travis environment)
-  #
-  # Set the following environment variables in the travis configuration (.travis.yml)
-  #
-  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
-  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
-  #   GIT_NAME         - The Git user name
-  #   GIT_EMAIL        - The Git user email
-  #
-  echo DEPLOY_BRANCH: $DEPLOY_BRANCH
-  echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
-  echo GIT_NAME: $GIT_NAME
-  echo GIT_EMAIL: $GIT_EMAIL
-  if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-    echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
+echo DEPLOY_BRANCH: $DEPLOY_BRANCH
+echo GIT_NAME: $GIT_NAME
+echo GIT_EMAIL: $GIT_EMAIL
+echo GHA_BRANCH: $GHA_BRANCH
+if [ "$GHA_BRANCH" != "$DEPLOY_BRANCH" ]; then
+    echo "Actions should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
     exit 0
-  else
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-      echo "Travis should not deploy from pull requests"
-      exit 0
-    else
-      # switch both git and https protocols as we don't know which travis
-      # is using today (it changed!)
-      REPO=${REPO/git:\/\/github.com\//git@github.com:}
-      REPO=${REPO/https:\/\/github.com\//git@github.com:}
-      
-      chmod 600 $SSH_KEY
-      eval `ssh-agent -s`
-      ssh-add $SSH_KEY
-      git config --global user.name "$GIT_NAME"
-      git config --global user.email "$GIT_EMAIL"
-    fi
-  fi
 fi
+
+REPO="https://RobinThrift:${GH_TOKEN}@github.com/RobinThrift/RobinThrift.com.git"
+
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
  
 REPO_NAME=$(basename $REPO)
 TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
 REV=$(git rev-parse HEAD)
 git clone --branch ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
-rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TARGET_DIR/
+rsync -rt --delete --exclude=".git" --exclude=".github" $SOURCE_DIR/ $TARGET_DIR/
 cd $TARGET_DIR
+echo "robinthrift.de" > CNAME
 git add -A .
-git commit --allow-empty -m "build(blog): build from $REV"
-git push $REPO $TARGET_BRANCH
-
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
+git commit --allow-empty -m "auto-build: build from $REV at $(date)"
+git remote add gh $REPO
+git push gh $TARGET_BRANCH
